f0d687ca534bacc167f291c8e6a618e23dc52035
Thiago Godoy Cunha <nhalykami@gmail.com>
2026-03-14 14:40:06 -0300

    Refinamento estético do código

--- Algoritmos/Atividade vivencial - 14.03.py
+++ Algoritmos/Atividade vivencial - 14.03.py
@@ -1,13 +1,11 @@
-x = 0
-while x == 0:
-    escolha = input("\n\n\nCalculadora master blaster:\nDigite 1 para somar dois valores\nDigite 2 para subtrair dois valores\nDigite 3 para multiplicar dois valores\nDigite 4 para dividir dois valores\nDigite 5 para realizar a potência entre dois valores\nDigite 6 para realizar a radiciação de dois valores\nDigite qualquer outro número para sair\n")
+while True:
+    escolha = input("Calculadora master blaster:\nDigite 1 para somar dois valores\nDigite 2 para subtrair dois valores\nDigite 3 para multiplicar dois valores\nDigite 4 para dividir dois valores\nDigite 5 para realizar a potência entre dois valores\nDigite 6 para realizar a radiciação de dois valores\nDigite qualquer outro número para sair\n")
     if escolha.isdigit():
         escolha = int(escolha)
 
         if escolha < 1 or escolha > 6:
             print("Tchau")
-            x = 1
-            continue
+            break
 
         num1 = input("Digite o valor do primeiro número: ")
         num2 = input("Digite o valor do segundo número: ")
@@ -19,20 +17,29 @@ while x == 0:
             print(f"O seu número é: ")
             if escolha == 1:
                 print(num1+num2)
+                print("\n\n\n")
             elif escolha == 2:
                 print(num1-num2)
+                print("\n\n\n")
             elif escolha == 3:
                 print(num1*num2)
+                print("\n\n\n")
             elif escolha == 4:
                 if num2 == 0:
                     print("Não pode dividir por 0")
+                    print("\n\n\n")
                 else:
                     print(num1/num2)
+                    print("\n\n\n")
             elif escolha == 5:
                 print(num1 ** num2)
+                print("\n\n\n")
             elif escolha == 6:
                 print(round(num1**(1/num2)))
+                print("\n\n\n")
         else:
             print("Digite um número!")
+            print("\n\n\n")
     else:
         print("Digite um número!")
+        print("\n\n\n")
